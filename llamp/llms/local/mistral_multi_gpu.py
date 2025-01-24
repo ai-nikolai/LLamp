95464fd7c7d96337d2a8ed27aaffb76e27717ed7
Nikolai Rozanov <nikolai.rozanov@gmail.com>
2025-01-24 15:39:17 +0000

    Mistral model added

--- llamp/llms/local/mistral_multi_gpu.py
+++ llamp/llms/local/mistral_multi_gpu.py
@@ -3,9 +3,9 @@ from vllm import LLM, SamplingParams
 import argparse
 
 
-class QwenLLMMultiGPU():
+class MistralMultiGPU():
 
-    def __init__(self, model_name="Qwen/Qwen2.5-32B-Instruct", tensor_parallel_size=3):
+    def __init__(self, model_name="mistralai/Mixtral-8x22B-v0.1", tensor_parallel_size=1):
         # Initialize the tokenizer
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
 
@@ -14,7 +14,7 @@ class QwenLLMMultiGPU():
         self.sampling_params = SamplingParams(temperature=0.7, top_p=0.8, repetition_penalty=1.05, max_tokens=512)
 
         # Input the model name or path. Can be GPTQ or AWQ models.
-        self.llm = LLM(model=model_name, tensor_parallel_size=tensor_parallel_size)
+        self.llm = LLM(model=model_name, tensor_parallel_size=tensor_parallel_size, dtype="auto")
         print("="*20)
         print(f"Model loaded as: {model_name} with tensors: {tensor_parallel_size}")
 
@@ -22,7 +22,7 @@ class QwenLLMMultiGPU():
         """Calls the llm and does stuff"""
         # Prepare your prompts
         messages = [
-            {"role": "system", "content": "You are Qwen, created by Alibaba Cloud. You are a helpful assistant."},
+            {"role": "system", "content": "You are Mixtral. You are a helpful assistant."},
             {"role": "user", "content": prompt}
         ]
         text = self.tokenizer.apply_chat_template(
@@ -42,10 +42,10 @@ class QwenLLMMultiGPU():
 
 
 if __name__ ==  "__main__":
-    parser = argparse.ArgumentParser(description='GPU Count for QWEN')
+    parser = argparse.ArgumentParser(description='GPU Count for Mistral')
     parser.add_argument('--gpus', type=int, default=1, help='Number of GPUs (default: 1)')
     args = parser.parse_args()
 
     prompt = "Tell me something about large language models."
-    llm = QwenLLMMultiGPU(tensor_parallel_size=args.gpus)
+    llm = MistralMultiGPU(tensor_parallel_size=args.gpus)
     llm.call_llm(prompt)
